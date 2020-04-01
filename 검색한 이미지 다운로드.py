980cec8373b539b3d3f2a5e4170cd2b365739dfd
Anchang-hyeon <61732687+Anchang-hyeon@users.noreply.github.com>
2020-04-01 17:50:41 +0900

    Update 검색한 이미지 다운로드.py

--- 검색한 이미지 다운로드.py
+++ 검색한 이미지 다운로드.py
@@ -5,21 +5,20 @@ from urllib.request import urlretrieve
 from urllib.parse import quote_plus
 
 a=input('검색어를 입력하세요.')
+#검색어 입력받음
 try:
     if not (os.path.isdir(a)):
-        os.makedirs(os.path.join(a))
+        os.makedirs(os.path.join(a))           #입력받은 검색어를 이름으로한 디렉토리생성
 except OSError as e:
     if e.errno != errno.EEXIST:
         print("폴더 생성 실패!")
         exit()
+       
 #웹 페이지를 열고 소스코드를 읽어오는 작업
 html = requests.get("https://search.naver.com/search.naver?where=image&sm=tab_jum&query="+quote_plus(a))
 soup = BeautifulSoup(html.text, 'html.parser')
 html.close()
-
-#요일별 웹툰영역 추출하기
 data1_list=soup.find('div',{'class':'photo_grid _box'})
-
 data2=data1_list.findAll('div',{'class':'img_area _item'})
 for x in range(len(data2)):
     img=data2[x].find('img')
